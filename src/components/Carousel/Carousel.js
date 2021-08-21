c04c0508024614daea423f036b13dcea22d65892
sunaycansev <fsunaycansev@gmail.com>
2021-08-21 17:41:21 +0300

    Product card ui change

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -6,16 +6,16 @@ function Slider() {
     <Container className="slider p-3">
       <Row>
         <Col>
-          <Carousel>
+          <Carousel className="position-relative">
             <Carousel.Item>
               <img
                 className="d-block w-100"
                 src="https://cdn.luxe.digital/media/2019/12/16162226/best-men-online-shopping-mr-porter-luxe-digital.jpg"
                 alt="First slide"
               />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>
+              <Carousel.Caption className="position-absolute top-0 start-0 text-start ps-5">
+                <h3 className="fs-1 font-monospace">First slide label</h3>
+                <p className="fs-2">
                   Nulla vitae elit libero, a pharetra augue mollis interdum.
                 </p>
               </Carousel.Caption>
@@ -27,9 +27,11 @@ function Slider() {
                 alt="Second slide"
               />
 
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <Carousel.Caption className="position-absolute top-0 end-0 text-end pe-5">
+                <h3 className="fs-1 font-monospace">Second slide label</h3>
+                <p className="fs-2">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -39,9 +41,9 @@ function Slider() {
                 alt="Third slide"
               />
 
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
+              <Carousel.Caption className="position-absolute top-0 start-0 text-start ps-3">
+                <h3 className="fs-1 font-monospace">Third slide label</h3>
+                <p className="fs-3">
                   Praesent commodo cursus magna, vel scelerisque nisl
                   consectetur.
                 </p>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,67 +3,82 @@ import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./_Product.scss";
 import { FiShoppingCart } from "react-icons/fi";
-import { BsHeart } from "react-icons/bs";
+import { BsHeartFill } from "react-icons/bs";
 import { FaHeart, FaRandom, FaStar, FaStarHalf } from "react-icons/fa";
 import { useCartContext } from "../../contexts/CartContext";
+
+function renderStars(number) {
+  const arr = new Array(number).fill(0);
+  return (
+    <ul className="rating p-0 m-0 mt-1 mb-3">
+      {arr.map((i, key) => (
+        // <li key={key} className="fas fa-star"></li>
+        <li key={key} className=" list-unstyled d-inline fs-4 text-warning">
+          <FaStar className="mt-3" />
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 const Product = ({ product }) => {
   const { image, title, price, id } = product;
   const { addToCart, removeCartItem } = useCartContext();
+  const secondaryImage =
+    "https://www.itopya.com/picture/razer-viper-mini-gaming-mouse-04.jpg";
   return (
-    <Col md={4} sm={6} lg={3} className="d-flex p-0">
-      <div className="product-grid shadow-sm rounded-1  justify-content-center align-self-center d-flex flex-column ">
-        <div className="product-image">
-          <Link to={`/product/${product.id}`} className="image">
-            <img className="pic-1 img-fluid " src={image} alt="img-1" />
+    <div key={id} className="column col-sm-6 col-md-4 col-lg-3 my-3">
+      <div
+        style={{ height: "100%" }}
+        className="product d-flex flex-column m-0  p-2 shadow-lg rounded"
+      >
+        <Link
+          className="text-decoration-none text-center text-dark"
+          to={`/product/${product.id}`}
+        >
+          <div className="top d-flex flex-column align-items-center pb-5 pt-3 position-relative">
+            {/* 33 ve hot */}
+            <span className="position-absolute top-0 end-0 badge rounded-pill bg-danger p-2 fs-6">
+              %33
+            </span>
+            <span className="position-absolute top-0 start-0 badge rounded-pill bg-success p-2 fs-6">
+              New
+            </span>
             <img
-              className="pic-2 img-fluid"
-              src="https://www.itopya.com/picture/razer-viper-mini-gaming-mouse-0.jpg"
-              alt="img-1"
+              style={{ height: 200, objectFit: "contain" }}
+              className="primary-image img-fluid"
+              src={image}
+              alt={title}
             />
-          </Link>
-          <span className="product-sale-label">hot</span>
-          <span className="product-discount-label">-30%</span>
-        </div>
-        <div className="product-content d-flex flex-column">
-          <ul className="rating">
-            <li className="fas fa-star">
-              <FaStar />
-            </li>
-            <li className="fas fa-star">
-              <FaStar />
-            </li>
-            <li className="fas fa-star">
-              <FaStar />
-            </li>
-            <li className="fas fa-star">
-              <FaStar />
-            </li>
-            <li className="fas fa-star disable">
-              <FaStar />
-            </li>
-          </ul>
-          <h3 className="title">
-            <a href={`/product/${product.id}`}>{title.slice(0, 40)}</a>
-          </h3>
-          <div className="price " style={{ color: "#ff6000" }}>
-            <span>$99.99</span>
-            {price}
+            <img
+              style={{ height: 200, objectFit: "contain" }}
+              className="secondary-image img-fluid"
+              src={secondaryImage}
+              alt={title}
+            />
+            {renderStars(5)}
+            <strong> {title} </strong>
           </div>
-          <div className="product-button-group mt-auto">
-            <a href="/#" className="product-like-icon">
-              <FaHeart />
-            </a>
-            <button className="add-to-cart" onClick={() => addToCart(product)}>
-              <FiShoppingCart /> Add To Cart
+        </Link>
+        <div className="bottom d-flex flex-column mt-auto justify-content-center my-2">
+          {/*<span className="justify-content-end">{price + 30}</span>*/}
+          <span className="text-center my-2 fs-4 text-success">${price}</span>
+          <div className="btn-group d-flex flex-column align-items-center">
+            <BsHeartFill className="icon fs-5" />
+            <button
+              className="btn  btn-md btn-dark"
+              onClick={() => addToCart(product)}
+            >
+              {/*<i className="cart-icon fas fa-shopping-cart me-2"></i>*/}
+              <FiShoppingCart className="me-2 cart-icon fs-4" />
+              Add to Card
             </button>
-            <a href="/#" className="product-compare-icon">
-              <FaRandom />
-            </a>
+            {/*<i className="icon right fas fa-random"></i>*/}
+            <FaRandom className="icon right fs-4" />
           </div>
         </div>
       </div>
-    </Col>
+    </div>
   );
 };
 export default Product;
