56c02caab1618b946889ba4e4be34d7aafba902f
sunaycansev <fsunaycansev@gmail.com>
2021-08-24 04:21:20 +0300

    Profile page add

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import { useAuthContext } from "../../contexts/AuthContext";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { useCartContext } from "../../contexts/CartContext";
+import Footer from "../../components/Footer/Footer";
+import { FiEdit3, FiLogOut, FiMail } from "react-icons/fi";
 function Profile({ history }) {
   const { user, logout } = useAuthContext();
+
   const { cart } = useCartContext();
   const handleLogout = async () => {
     console.log("ddsa");
@@ -16,12 +19,61 @@ function Profile({ history }) {
   return (
     <React.Fragment>
       <Header />
-      <h1>profil bilgileri</h1>
-      <code>{JSON.stringify(cart)}</code>
-
-      <Button variant="danger" onClick={handleLogout}>
-        Logout
-      </Button>
+      <Container className="d-flex justify-content-center align-items-center py-4 my-4">
+        <Row>
+          <Col className="d-flex justify-content-center align-items-center">
+            <div className="card p-4 px-5 shadow-lg" style={{ width: "25rem" }}>
+              <img
+                src="https://picsum.photos/300"
+                className="card-img-top rounded-circle"
+                alt="avatar"
+              />
+              <h3 className="text-center mt-3">Alice Sterling</h3>
+              <div className="card-body d-flex justify-content-between">
+                <button className="card-link btn btn-sm btn-info px-3 ">
+                  Edit Profile
+                  <span>
+                    <FiEdit3 className="ms-3" />
+                  </span>{" "}
+                  <br />
+                </button>
+                <button className="card-link btn btn-sm btn-danger px-3">
+                  Logout
+                  <span>
+                    <FiLogOut className="ms-3" />
+                  </span>
+                </button>
+              </div>
+              <div className="card-body">
+                <h5 className="card-title text-center fw-bolder">User Infos</h5>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item d-flex flex-column">
+                  <span className="text-center d-block fw-bold">
+                    {" "}
+                    <span>
+                      <FiMail className="me-2" />
+                    </span>
+                    E-mail
+                  </span>{" "}
+                  {user?.email}
+                </li>
+                <li className="list-group-item d-flex flex-column">
+                  <span className="text-center d-block fw-bold">
+                    {" "}
+                    <span>
+                      <FiMail className="me-2" />
+                    </span>
+                    Password
+                  </span>{" "}
+                  *******
+                </li>
+              </ul>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
     </React.Fragment>
   );
 }
